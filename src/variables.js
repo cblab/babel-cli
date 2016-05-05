b0aafd9ed19c78d6603c79a19001c0a318191c90
Christian Blab <chris.blab@gmail.com>
2016-05-05 09:21:49 +0200

    variables - old style

---
+++ src/variables.js
@@ -0,0 +1,11 @@
+function fire(bool) {
+    var foo;
+    if (bool) {
+        foo = 'bar';
+        console.log(foo); // bar
+    } else {
+        console.log(foo); //undefined -> hoisting (quite confusing, isn't it?)
+    }
+}
+
+fire(false);
